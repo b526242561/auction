14f284d4cff3e3f76546a9eb6f652602e8e68f51
b526242561 <526242561@qq.com>
2017-04-17 13:28:32 +0800

    更新
    增加产品路由

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = '慕课网Angular入门实战';
+  title = 'xxxxx';
 }
---
+++ src/app/product-detail/product-detail.component.js
@@ -0,0 +1,27 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+exports.__esModule = true;
+var core_1 = require("@angular/core");
+var ProductDetailComponent = (function () {
+    function ProductDetailComponent(routerInfo) {
+        this.routerInfo = routerInfo;
+    }
+    ProductDetailComponent.prototype.ngOnInit = function () {
+        this.productTitle = this.routerInfo.snapshot.params["prodTitle"];
+    };
+    return ProductDetailComponent;
+}());
+ProductDetailComponent = __decorate([
+    core_1.Component({
+        selector: 'app-product-detail',
+        templateUrl: './product-detail.component.html',
+        styleUrls: ['./product-detail.component.css']
+    })
+], ProductDetailComponent);
+exports.ProductDetailComponent = ProductDetailComponent;
+//# sourceMappingURL=product-detail.component.js.map
